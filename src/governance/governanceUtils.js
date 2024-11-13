220c3a9329c0b4cbcf2150a013422ab15f6920e4
KOSASIH <kosasihg88@gmail.com>
2024-11-13 22:23:13 +0700

    Create governanceUtils.js

---
+++ src/governance/governanceUtils.js
@@ -0,0 +1,6 @@
+class GovernanceUtils {
+    // Utility functions for governance processes can be added here
+    // For now, we can keep this file for future enhancements
+}
+
+module.exports = new GovernanceUtils();
